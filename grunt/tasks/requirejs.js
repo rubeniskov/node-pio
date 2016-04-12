8ec09cfa7370fb6a4b478e3d5c3c6381b5f760e2
Rubén López Gómez <loslopez1@icloud.com>
2016-04-12 07:43:18 +0200

    update: grunt-requirejs configuration

--- grunt/tasks/requirejs.js
+++ grunt/tasks/requirejs.js
@@ -1,14 +1,16 @@
 const   path = require('path');
 
 module.exports = function(grunt, data) {
-    var dist    = Object.keys(data.app.config["app-js"])[0],
-        source  = data.app.config["app-js"][dist];
+    var dist    = Object.keys(data.app.config.build.js)[0],
+        source  = data.app.config.build.js[dist];
     return {
-        "compile": {
+        "build": {
             options: {
                 baseUrl: path.dirname(source),
                 paths: '<%= app.config.paths %>',
                 shim: '<%= app.config.shim %>',
+                optimize: "uglify2",
+
                 wrap: {
                     start: "(function() {",
                     end: "}());"
