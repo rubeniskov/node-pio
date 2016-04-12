77d1cb27c717eb03f4cdb94c2d91bf0ec3a852ea
Rubén López Gómez <loslopez1@icloud.com>
2016-04-12 07:41:21 +0200

    update: improve Gruntfile configuration

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,13 @@
-const   fs = require('fs');
-
 module.exports = function(grunt) {
     require('time-grunt')(grunt);
     require('load-grunt-config')(grunt, {
         configPath: require('path').join(process.cwd(), 'grunt/tasks'),
         data: {
             app: {
-                name : grunt.package.name,
-                version : grunt.package.version,
-                config: JSON.parse(fs.readFileSync('./config.json', 'utf8')).app
+                name : '<%= package.name %>',
+                version : '<%= package.version %>',
+                config: grunt.file.readJSON('./config.json', 'utf8').app,
+                package : '<%= package %>',
             }
         }
     });
