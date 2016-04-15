3a01fe3d715b5eda6f7dcb5e50b6e267ec16c31d
Rubén López Gómez <loslopez1@icloud.com>
2016-04-15 06:11:54 +0200

    update: angular config

--- app/script/config.js
+++ app/script/config.js
@@ -1,5 +1,24 @@
-define([], function(){
-    return {
-        
-    }
-})
+define(['app', 'route', 'i18n'], function(app, route, i18n) {
+    return app
+        .config(function(
+            $stateProvider,
+            $urlRouterProvider,
+            $resourceProvider,
+            $translateProvider,
+            $httpProvider,
+            localStorageServiceProvider) {
+
+            $resourceProvider.defaults.stripTrailingSlashes = false;
+
+            $httpProvider.interceptors.push('jwtInterceptor');
+
+            localStorageServiceProvider
+                .setPrefix(app.name)
+                .setStorageType('localStorage');
+
+            route($stateProvider, $urlRouterProvider);
+
+            i18n($translateProvider);
+
+        });
+});
