9f8e9f66c08d9b520504e2201b0ddc2f073e99ea
Rubén López Gómez <loslopez1@icloud.com>
2016-04-15 06:11:46 +0200

    update: angular app.js

--- app/script/app.js
+++ app/script/app.js
@@ -1,9 +1,36 @@
-define(["angular"], function(angular){
-    return angular.module('pollsio', [])
-        // .run([], function(){
-        //
-        // });
-            // .factory('ideasDataSvc',ideasDataSvc);
-            // .controller('ideasHomeController', ideasHomeController);
-            // .controller('ideaDetailsController',ideaDetailsController);
+define([
+    'angular',
+    'angular-sanitize',
+    'angular-resource',
+    'angular-translate',
+    'angular-ui-router',
+    'angular-bootstrap',
+    'angular-oclazyload',
+    'angular-local-storage',
+    'angular-md5',
+    'angular-url-parser'
+], function(angular) {
+    return angular.module('pio', [
+            'ngSanitize',
+            'ngResource',
+            'pascalprecht.translate',
+            'ui.router',
+            'ui.bootstrap',
+            'oc.lazyLoad',
+            'LocalStorageModule',
+            'angular-md5',
+            'urlParser'
+        ])
+        .run(function($rootScope, $state) {
+            $rootScope.$state = $state;
+
+            $rootScope.$on('$stateChangeStart', function(e, to) {
+                if (to.data && to.data.requiresLogin) {
+                    if (!store.get('jwt') || jwtHelper.isTokenExpired(store.get('jwt'))) {
+                        e.preventDefault();
+                        $state.go('login');
+                    }
+                }
+            });
+        });
 });
