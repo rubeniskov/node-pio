54fd1bd085580413812fdd3206b6d7bd31a1c513
Rubén López Gómez <loslopez1@icloud.com>
2016-04-15 06:03:24 +0200

    add: partials static server endpoint

---
+++ server/partial.js
@@ -0,0 +1,9 @@
+const   express = require('express'),
+        path = require('path'),
+        router = express.Router();
+
+module.exports = function(app, opts, cfg, utils){
+    return router.get('/partial/:filename', function(req, res, next) {
+        utils.sendFile('partials/'+req.params.filename, req, res, next);
+    });
+};
