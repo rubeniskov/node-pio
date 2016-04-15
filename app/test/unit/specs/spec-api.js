262ee44b720a93a2d0b7f2536df65d9f22fb43d9
Rubén López Gómez <loslopez1@icloud.com>
2016-04-15 06:07:37 +0200

    update: commented unit test spec-api

--- app/test/unit/specs/spec-api.js
+++ app/test/unit/specs/spec-api.js
@@ -2,9 +2,45 @@
 
 describe('Api Service', function() {
 
-    beforeEach(angular.module('pio'));
+    var scope = {}
 
-    it('should have a title', function() {
-        assert.equal('Super Calculator', 'Super Calculator');
+    beforeEach(module('pio'));
+
+    beforeEach(inject(function($injector) {
+        scope.apiService = $injector.get('apiService');
+        scope.$q = $injector.get('$q');
+        scope.$httpBackend = $injector.get('$httpBackend');
+    }));
+
+    it('should be defined', function() {
+        expect(scope.apiService).to.be.ok;
+    });
+
+    describe('User management', function(apiService) {
+
+        it('should be defined ', function() {
+            expect(scope.apiService.createUser).to.be.ok;
+        });
+
+        // it('should get an user ', function(apiService) {
+        //     expect(apiService.getUser).to.be.ok;
+        //
+        //     var getUserSpy = sinon.spy();
+        //     expect(apiService.getUser).to.be.ok;
+        // });
+        //
+        // it('should update an user ', function(apiService) {
+        //     expect(apiService.getUser).to.be.ok;
+        //
+        //     var getUserSpy = sinon.spy();
+        //     expect(apiService.getUser).to.be.ok;
+        // });
+        //
+        // it('should delete an user ', function(apiService) {
+        //     expect(apiService.getUser).to.be.ok;
+        //
+        //     var getUserSpy = sinon.spy();
+        //     expect(apiService.getUser).to.be.ok;
+        // });
     });
 });
