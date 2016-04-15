37331faaa0a955b17135b69f9f0f520b0807138d
Rubén López Gómez <loslopez1@icloud.com>
2016-04-15 06:44:52 +0200

    update: route change doublequote to singlequote

--- app/script/route.js
+++ app/script/route.js
@@ -4,26 +4,26 @@ define([], function() {
         $stateProvider
             .state('index', {
                 abstract: true,
-                url: "/index",
-                templateUrl: "views/common/content.html",
+                url: '/index',
+                templateUrl: 'views/common/content.html',
             })
             .state('home.main', {
-                url: "/main",
-                templateUrl: "view/main.html",
+                url: '/main',
+                templateUrl: 'view/main.html',
                 data: {
                     pageTitle: 'Home'
                 }
             })
             .state('home.minor', {
-                url: "/minor",
-                templateUrl: "view/minor.html",
+                url: '/minor',
+                templateUrl: 'view/minor.html',
                 data: {
                     pageTitle: 'Example view'
                 }
             })
             .state('sign-in', {
-                url: "/sign-in",
-                templateUrl: "view/sign-in.html",
+                url: '/sign-in',
+                templateUrl: 'view/sign-in.html',
                 data: {
                     pageTitle: 'Sign In',
                     specialClass: 'gray-bg'
@@ -31,6 +31,6 @@ define([], function() {
             })
 
         $urlRouterProvider
-            .otherwise("/home");
+            .otherwise('/home');
     };
 });
