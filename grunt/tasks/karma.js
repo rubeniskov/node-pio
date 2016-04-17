53ba8620f64e0ce0b15ad64a4c08957e5934bac5
Rubén López Gómez <loslopez1@icloud.com>
2016-04-18 01:36:15 +0200

    update: grunt karma unit test

--- grunt/tasks/karma.js
+++ grunt/tasks/karma.js
@@ -1,16 +1,52 @@
+// http://webapplog.com/tdd/
+// http://www.yearofmoo.com/2013/01/full-spectrum-testing-with-angularjs-and-karma.html
 
 module.exports = function(grunt, factory) {
     return {
         options: {
-            configFile: "app/test/unit/cfg.js",
+            basePath: '',
+            frameworks: ['jasmine', 'sinon-chai'],
+            logLevel: 'DEBUG',
+            files: [
+                'app/dist/bundle.libs.js',
+                'app/dist/bundle.main.js',
+                'app/test/unit/specs/spec-*.js',
+                'app/test/unit/plugins/karma-angular-bootstrap/index.js',
+                'app/test/unit/plugins/karma-define-amd/index.js'
+            ],
+            reporters: ['progress', 'coverage'],
+            preprocessors: {
+                'app/build/main.js': ['coverage']
+            },
+            colors: true,
+            autoWatch: true,
+            captureTimeout: 60000,
             seleniumConfig: {
-                logLevel: 'DEBUG', // LOG_DEBUG,
                 hostname: '<%= utils.getLocalhostAddress() %>',
                 port: 4444,
                 browser: {
                     hostname: '<%= utils.getSeleniumGatewayAddress() %>'
                 }
-            }
+            },
+            customLaunchers: {
+                'chrome': {
+                    browserName: 'chrome',
+                    base: 'SeleniumWebDriver'
+                },
+                'firefox': {
+                    browserName: 'firefox',
+                    base: 'SeleniumWebDriver'
+                }
+            },
+            coverageReporter: {
+                dir: 'app/test/unit/coverage/',
+                reporters: [
+                    { type: 'html', subdir: '.' },
+                    { type: 'text-summary', subdir: '.', file: 'summary.txt' },
+                    { type: 'cobertura', subdir: '.', file: 'cobertura.txt' }
+                ]
+            },
+            browsers: ['chrome', 'firefox']
         },
         "ci-unit": {
             options: {
