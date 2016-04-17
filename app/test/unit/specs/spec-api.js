8ec11fbc4fad092ba04efeccd0d4ba6469cb7143
Rubén López Gómez <loslopez1@icloud.com>
2016-04-18 01:47:09 +0200

    update: unit test api

--- app/test/unit/specs/spec-api.js
+++ app/test/unit/specs/spec-api.js
@@ -1,46 +1,54 @@
-// http://webapplog.com/tdd/
-
 describe('Api Service', function() {
 
-    var scope = {}
+    var suite = {}
 
-    beforeEach(module('pio'));
+    beforeEach(angular.mock.module('pio'));
 
-    beforeEach(inject(function($injector) {
-        scope.apiService = $injector.get('apiService');
-        scope.$q = $injector.get('$q');
-        scope.$httpBackend = $injector.get('$httpBackend');
+    beforeEach(angular.mock.inject(function($injector) {
+        suite.apiService = $injector.get('apiService');
+        suite.$q = $injector.get('$q');
+        suite.$httpBackend = $injector.get('$httpBackend');
     }));
 
-    it('should be defined', function() {
-        expect(scope.apiService).to.be.ok;
+    afterEach(function () {
+      // NOTE: prevents DOM elements leak
+      //suite.element.remove();
+    });
+    afterAll(function () {
+      // NOTE: prevents memory leaks because of JavaScript closures created for
+      // jasmine syntax (beforeEach, afterEach, beforeAll, afterAll, it..).
+      suite = null;
     });
 
-    describe('User management', function(apiService) {
+    // it('should be defined', function() {
+    //     expect(suite.apiService).toBeDefined();
+    // });
 
-        it('should be defined ', function() {
-            expect(scope.apiService.createUser).to.be.ok;
-        });
-
-        // it('should get an user ', function(apiService) {
-        //     expect(apiService.getUser).to.be.ok;
-        //
-        //     var getUserSpy = sinon.spy();
-        //     expect(apiService.getUser).to.be.ok;
-        // });
-        //
-        // it('should update an user ', function(apiService) {
-        //     expect(apiService.getUser).to.be.ok;
-        //
-        //     var getUserSpy = sinon.spy();
-        //     expect(apiService.getUser).to.be.ok;
-        // });
-        //
-        // it('should delete an user ', function(apiService) {
-        //     expect(apiService.getUser).to.be.ok;
-        //
-        //     var getUserSpy = sinon.spy();
-        //     expect(apiService.getUser).to.be.ok;
-        // });
-    });
+    // describe('User management', function() {
+    //
+    //     it('should be defined ', function() {
+    //         expect(suite.apiService).toBeDefined();
+    //     });
+    //
+    //     // it('should get an user ', function(apiService) {
+    //     //     expect(apiService.getUser).to.be.ok;
+    //     //
+    //     //     var getUserSpy = sinon.spy();
+    //     //     expect(apiService.getUser).to.be.ok;
+    //     // });
+    //     //
+    //     // it('should update an user ', function(apiService) {
+    //     //     expect(apiService.getUser).to.be.ok;
+    //     //
+    //     //     var getUserSpy = sinon.spy();
+    //     //     expect(apiService.getUser).to.be.ok;
+    //     // });
+    //     //
+    //     // it('should delete an user ', function(apiService) {
+    //     //     expect(apiService.getUser).to.be.ok;
+    //     //
+    //     //     var getUserSpy = sinon.spy();
+    //     //     expect(apiService.getUser).to.be.ok;
+    //     // });
+    // });
 });
