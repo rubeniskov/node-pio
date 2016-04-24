708a39c4b24bd398399816a4ef4ab36604bd0b79
Rubén López Gómez <loslopez1@icloud.com>
2016-04-24 23:32:24 +0200

    add: authProvider

---
+++ app/src/factories/authProvider.js
@@ -0,0 +1,22 @@
+define(['app', 'crypto-js'], function(app, crypto) {
+    app.factory('authProvider', function(API_CONFIG, API_URL, $http, $q, jwtProvider) {
+
+        return {
+            authenticate: function(id, password) {
+                var deferred = $q.defer();
+                $http.post(API_URL + '/authenticate', { hash: crypto.AES.encrypt(JSON.stringify({
+                    id: id,
+                    password: password
+                }), API_CONFIG.key).toString()}).then(function(response){
+                    jwtProvider.setToken(response.data.token);
+                    deferred.resolve();
+                }, deferred.reject);
+                return deferred.promise;
+            },
+            revoke: function(){
+                jwtProvider.removeToken();
+                return $q.resolve();
+            }
+        }
+    });
+});
