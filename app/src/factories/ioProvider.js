afee59f0bbcbb1aba556ff253bc017279355ba9a
Rubén López Gómez <loslopez1@icloud.com>
2016-04-24 23:32:06 +0200

    update: ioProvider switch session token

--- app/src/factories/ioProvider.js
+++ app/src/factories/ioProvider.js
@@ -1,14 +1,27 @@
 define(['app', 'socket-io'], function(app, io) {
     app.factory('ioProvider', function(socketFactory, jwtProvider) {
-        return socketFactory({
-            ioSocket: io.connect('http://' + window.location.hostname + ':' + window.location.port + '/', {
-                'query': 'token=' + jwtProvider.getToken()
-            }).on('error', function(error) {
-                if (error.type === 'UnauthorizedError' || error.code === 'invalid_token') {
-                    // redirect user to login page perhaps?
-                    console.log('User\'s token has expired');
-                }
-            })
+
+        var _io = io.connect('http://' + window.location.hostname + ':' + window.location.port + '/', {
+            'query': 'token=' + jwtProvider.getToken(),
+            'reconnection delay': 1000,
+            'reconnection limit': 1000,
+            'max reconnection attempts': 'Infinity'
+        })
+        .on('connect', function(socket){
+            console.log('IO - Connection');
+        })
+        .on('error', function(error) {
+            if (error.type === 'UnauthorizedError' || error.code === 'invalid_token') {
+                // redirect user to login page perhaps?
+                console.log('User\'s token has expired');
+            }
         });
+
+        return {
+            io: _io,
+            socket: socketFactory({
+                ioSocket: _io
+            })
+        }
     });
 });
