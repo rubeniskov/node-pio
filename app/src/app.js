81f44a6a7c4d854cc6e2b889f27ae6c9ad484102
Rubén López Gómez <loslopez1@icloud.com>
2016-04-25 04:56:42 +0200

    add: ladda angular include

--- app/src/app.js
+++ app/src/app.js
@@ -6,6 +6,7 @@ define(['angular',
     'angular-peity',
     'angular-loading-bar',
     'angular-jwt',
+    'angular-ladda',
     'angular-ui-router',
     'angular-bootstrap',
     'angular-oclazyload',
@@ -24,6 +25,7 @@ define(['angular',
             'angular-peity',
             'angular-loading-bar',
             'angular-jwt',
+            'angular-ladda',
             'ui.router',
             'ui.bootstrap',
             'oc.lazyLoad',
@@ -34,12 +36,26 @@ define(['angular',
             'btford.socket-io',
             'duScroll'
         ])
-        .run(function($rootScope, $state, jwtProvider) {
+        .run(function($rootScope, $state, $timeout, authService) {
+
             $rootScope.$state = $state;
+
+            $rootScope.$on('$authTokenExpired', function($event, auth){
+                $timeout(function(){
+                    $state.go('sign-in');
+                }, 600);
+            });
+
+            $rootScope.$on('$authTokenProvided', function($event, auth){
+                $timeout(function(){
+                    $state.go($state.current, {}, {reload: true});
+                }, 600);
+            });
+
             $rootScope.$on('$stateChangeStart', function($event, to) {
                 if (to.data) {
                     if (to.data.requiresLogin) {
-                        if (jwtProvider.isTokenExpired()) {
+                        if (!authService.isSigned()) {
                             $event.preventDefault();
                             $state.go('sign-in');
                         }
